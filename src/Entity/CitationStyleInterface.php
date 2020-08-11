db3cbfe9a72954c0a411e47943be3b684282d201
Bryan J. Brown <bjbrown@fsu.edu>
2020-08-11 16:53:24 -0400

    Adds new config entities for citation styles and entity metadata mapping

---
+++ src/Entity/CitationStyleInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\citations\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for defining Citation style entities.
+ */
+interface CitationStyleInterface extends ConfigEntityInterface {
+
+  // Add get/set methods for your configuration properties here.
+}
---
+++ src/Entity/EntityMetadataMappingInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\citations\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for defining Entity metadata mapping entities.
+ */
+interface EntityMetadataMappingInterface extends ConfigEntityInterface {
+
+  // Add get/set methods for your configuration properties here.
+}
